102343798de16d57617f37431c919a87ac9e4712
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-01-04 14:05:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function removeSelectedColor() 
+{
+      const colorSelect = document.getElementById('colorSelect');
+      // Remove the selected option from the dropdown
+      colorSelect.remove(colorSelect.selectedIndex);
+}
